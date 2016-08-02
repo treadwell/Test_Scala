b3101d00deb616abab7da5c01a65c433af2e981d
treadwell <brooks.ken@gmail.com>
2016-08-02 14:40:08 -0400

    Add jdbc

---
+++ src/HelloWorld.scala
@@ -0,0 +1,12 @@
+/**
+  * Created by kbrooks on 8/1/16.
+  */
+object HelloWorld {
+  /* This is my first java program.
+  * This will print 'Hello World' as the output
+   */
+  def main(args: Array[String]): Unit = {
+    println("Hello World!") // prints Hello World
+  }
+}
+
